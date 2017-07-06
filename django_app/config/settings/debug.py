c231d3d21264f835ea4b8f91716671d171c18b68
DahamLee <daham1224@gmail.com>
2017-07-06 19:09:22 +0900

    phone_number field 추가했습니다 pip install로 다운받아야 되서 deploy, debug세팅 모두 추가하겠습니다

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -6,6 +6,7 @@ config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
 INSTALLED_APPS.append('django_extensions')
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
